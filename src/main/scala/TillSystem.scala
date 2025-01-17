2d69f45ac038eace68fbf8aa2e614e82ea963c37
MJANOWICZ <michal.janowicz@digital.voa.gov.uk>
2025-01-17 06:39:13 +0000

    02 Task1

--- src/main/scala/TillSystem.scala
+++ src/main/scala/TillSystem.scala
@@ -1,3 +1,14 @@
-object TillSystem {
+object TillSystem   {
+case class MenuItem(name: String, price: Double)
 
+val menu: List[MenuItem] = List(
+  MenuItem("Cola", 0.50),
+  MenuItem("Coffee", 1.00),
+  MenuItem("Cheese sandwich", 2.00),
+  MenuItem("Steak sandwich", 4.50)
+)
+
+def calculatePrice(items: List[MenuItem]): Double = {
+  items.map(_.price).sum
+}
 }
--- src/test/scala/TillSystemSpec.scala
+++ src/test/scala/TillSystemSpec.scala
@@ -1,11 +1,16 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import TillSystem._
 
 class TillSystemSpec extends AnyFlatSpec with Matchers {
 
   "System" should "calculate correct price of items" in {
-    val items = List(1, 2)
-    val total = items.sum
-    total shouldBe 3
+    val items = List(
+      MenuItem("Cola", 0.50),
+      MenuItem("Coffee", 1.00),
+      MenuItem("Cheese", 2.00)
+    )
+    val total = calculatePrice(items)
+    total shouldBe 3.50
   }
 }
