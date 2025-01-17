13156cb1e28e1966bd17ca8901ba59d4510f7f53
MJANOWICZ <michal.janowicz@digital.voa.gov.uk>
2025-01-17 07:28:37 +0000

    03 task2

--- src/main/scala/TillSystem.scala
+++ src/main/scala/TillSystem.scala
@@ -1,14 +1,29 @@
-object TillSystem   {
-case class MenuItem(name: String, price: Double)
+object TillSystem {
+  case class MenuItem(
+                       name: String,
+                       price: Double,
+                       isFood: Option[Boolean] = None,
+                       isHotFood: Option[Boolean] = None)
 
-val menu: List[MenuItem] = List(
-  MenuItem("Cola", 0.50),
-  MenuItem("Coffee", 1.00),
-  MenuItem("Cheese sandwich", 2.00),
-  MenuItem("Steak sandwich", 4.50)
-)
+  val menu: List[MenuItem] = List(
+    MenuItem("Cola", 0.50),
+    MenuItem("Coffee", 1.00),
+    MenuItem("Cheese sandwich", 2.00, Some(true)),
+    MenuItem("Steak sandwich", 4.50, Some(true), Some(true))
+  )
 
-def calculatePrice(items: List[MenuItem]): Double = {
-  items.map(_.price).sum
-}
+  def calculatePrice(items: List[MenuItem]): Double = {
+    items.map(_.price).sum
+  }
+
+  def calculatePriceWithTip(items: List[MenuItem]): Double = {
+    val priceWithNoTip = calculatePrice(items)
+    val hasHotFood = items.exists(_.isHotFood.getOrElse(false))
+    val hasFood = items.exists(_.isFood.getOrElse(false))
+    val tipPercent = if (hasHotFood) 0.2 else if (hasFood) 0.1 else 0.0
+    val totalTip = priceWithNoTip * tipPercent
+    val finalTip = if (totalTip > 20) 20 else totalTip
+
+    priceWithNoTip + finalTip
+  }
 }
--- src/test/scala/TillSystemSpec.scala
+++ src/test/scala/TillSystemSpec.scala
@@ -4,13 +4,35 @@ import TillSystem._
 
 class TillSystemSpec extends AnyFlatSpec with Matchers {
 
+  val cola: MenuItem =   MenuItem("Cola", 0.50)
+  val coffee: MenuItem = MenuItem("Coffee", 1.00)
+  val cheese: MenuItem = MenuItem("Cheese sandwich", 2.00, isFood = Option(true))
+  val steak: MenuItem =  MenuItem("Steak sandwich", 4.5, isFood = Option(true), isHotFood = Option(true))
+
   "System" should "calculate correct price of items" in {
-    val items = List(
-      MenuItem("Cola", 0.50),
-      MenuItem("Coffee", 1.00),
-      MenuItem("Cheese", 2.00)
-    )
+    val items = List(cola, coffee, cheese)
     val total = calculatePrice(items)
     total shouldBe 3.50
+  }
+  "System" should "calculate tip and final bill for drinks only" in {
+    val items = List(coffee, cola)
+    val total = calculatePriceWithTip(items)
+    total shouldBe 1.5
+  }
+  "System" should "calculate tip and final bill for food" in {
+    val items = List(coffee, cola, cheese)
+    val total = calculatePriceWithTip(items)
+    total shouldBe 3.85
+  }
+  "System" should "calculate tip and final bill for hot food" in {
+    val items = List(steak, steak)
+    val total = calculatePriceWithTip(items)
+    total shouldBe 10.8
+  }
+  "System" should "calculate tip and final bill for hot food with tip cap" in {
+    val items = List.fill(30)(steak) // should be 155
+
+    val total = calculatePriceWithTip(items)
+    total shouldBe 155
   }
 }
